f7909535f18cb5a967bd371b54e53f1edcfeb48a
JuergenSchwertl <jschwert@microsoft.com>
2021-07-29 16:19:53 +0200

    added Doxygen style comments

--- SphereBME280/BMP280/Inc/libBMP280.h
+++ SphereBME280/BMP280/Inc/libBMP280.h
@@ -10,19 +10,18 @@ typedef struct BMP280_DATA {
 } bmp280_data_t;
 
 
-///<summary>
-///Initialize BMP280 sensor at device address
-///</summary>
-///<param name="i2cInterfaceFd">Interface Id of Azure Sphere I2C ISU block</param>
-///<param name="onPrimaryI2CAddress">use primary I2C bus address of BMP 280 sensor</param>
-///<returns>true if successful, false if error</returns>
+/// @brief Initialize BMP280 sensor at device address
+///
+/// @param i2cInterfaceFd		Interface Id of Azure Sphere I2C ISU block
+/// @param onPrimaryI2CAddress	use primary I2C bus address of BMP 280 sensor
+/// @returns true if successful, false if error
 bool BMP280_Init(int i2cInterfaceFd, bool onPrimaryI2CAddress);
 
-///<summary>
+/// @brief 
 /// Reads temperature [degC], pressure [Pa] from BMP280 sensor
-///</summary>
-///<param name="pData">pointer to <see href="bmp280_data_t">bmp280_data_t</see> structure receiving output</param>
-///<returns>0 if successful, -1 if error</returns>
+///
+/// @param		pData	pointer to @see bmp280_data_t structure receiving output
+/// @returns	0 if successful, -1 if error</returns>
 int BMP280_GetSensorData(bmp280_data_t *pData);
 
 
